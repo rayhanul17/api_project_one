8af874bd903f2680c8b93fc0849cd2f905fb72d7
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-10-25 00:30:28 +0600

    fix & check .yml file

--- SampleApi/SampleApi.Api/Controllers/WeatherForecastController.cs
+++ SampleApi/SampleApi.Api/Controllers/WeatherForecastController.cs
@@ -23,15 +23,13 @@ namespace SampleApi.Api.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            var list =  Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-
-            return list;
         }
     }
 }
